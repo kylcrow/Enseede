d29d7a41774daa104dd76e93bcee1a471af96d7d
Kyle Crow <kcrow@assurerx.local>
2016-03-08 16:46:56 -0500

    adding basic javascript code to load a title screen.

--- kylcrow.js
+++ kylcrow.js
@@ -1,34 +1,37 @@
+/// <reference path="assets/easeljs.d.ts" />
 var canvas;
 var stage;
-
 var main;
+var imgCrow;
+var screenWidth = 480;
+var screenHeight = 320;
 
-var tkr = new Object;
-
-//preloader
-var preloader;
 var manifest;
 var totalLoaded = 0;
 
-var titleView = new Container();
+var titleView = new createjs.Container();
 
 function Main()
 {
     canvas = document.getElementById('EnSeeDeStage');
-    stage = new Stage(canvas);
+    stage = new createjs.Stage(canvas);
     
     stage.mouseEventsEnabled = true;
+
+    manifest = [
+                { src:"assets/images/CrowSprite.png", id:"imgCrow"}
+            ];
+
+    var queue = new createjs.LoadQueue(false);
     
-    preloader = new PreloadJS();
-    preloader.onProgress = handleProgress;
-    preloader.onComplete = handleComplete;
-    preloader.onFileLoad = handleFileLoad;
-    preloader.loadManifest(manifest);
- 
-    /* Ticker */
-     
-    Ticker.setFPS(30);
-    Ticker.addListener(stage);
+    queue.on("complete", handleComplete, this);
+    queue.on("progress", handleProgress, this);
+    queue.on("fileload", handleFileLoad, this);
+    queue.loadManifest(manifest);
+
+    /* Ticker */     
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener("tick", stage);
 }
 
 function handleProgress(event)
@@ -36,53 +39,53 @@ function handleProgress(event)
     //use event.loaded to get the percentage of the loading
 }
  	
+function handleComplete(event)
+{
+    //use event.loaded to get the percentage of the loading
+}
+
 function handleLoadComplete(event) 
 {
- 
-   totalLoaded++;
+    totalLoaded++;
     
-   if (manifest.length == totalLoaded)
-   {
-       addTitleView();
-   }
+    if (manifest.length == totalLoaded)
+    {
+        AddTitleView();
+    }
 } 
 
-function handleFileLoad(event) {
-         //triggered when an individual file completes loading
-             
-         switch(event.type)
-         {
-            case PreloadJS.IMAGE:
-            //image loaded
-             var img = new Image();
-              img.src = event.src;
-              img.onload = handleLoadComplete;
-              window[event.id] = new Bitmap(img);
+function handleFileLoad(event) 
+{
+     var item = event.item; // A reference to the item that was passed in to the LoadQueue
+     var type = item.type;
+
+    //triggered when an individual file completes loading        
+    switch (type)
+    {
+     case createjs.LoadQueue.IMAGE:
+        //image loaded
+            var img = new Image();
+            img.src = item.src;
+            img.onload = handleLoadComplete;
+            window[item.id] = new createjs.Bitmap(img);
             break;
- 
-            case PreloadJS.SOUND:
+
+        case createjs.LoadQueue.SOUND:
             //sound loaded
             handleLoadComplete();
             break;
-         }
+        default:
+            break;
+    }
 }
 
-function addTitleView()
+function AddTitleView()
 {
-    //console.log("Add Title View");
-    startB.x = 240 - 31.5;
-    startB.y = 160;
-    startB.name = 'startB';
-     
-    creditsB.x = 241 - 42;
-    creditsB.y = 200;
-     
-    TitleView.addChild(main, startB, creditsB);
-    stage.addChild(bg, TitleView);
+    imgCrow.x = (screenWidth / 2) - (imgCrow.image.width / 2);
+    imgCrow.y = (screenHeight / 2) - (imgCrow.image.height / 2);
+    imgCrow.name = 'imgCrow';
+
+    titleView.addChild(main, imgCrow);
+    stage.addChild(imgCrow, titleView);
     stage.update();
-     
-    // Button Listeners
-     
-    startB.onPress = tweenTitleView;
-    creditsB.onPress = showCredits;
 }
